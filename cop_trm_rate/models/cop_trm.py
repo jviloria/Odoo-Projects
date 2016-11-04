0bbe335d7b309530c7f517a9e6e60f64c1d3bce0
John W. Viloria Amaris <john.viloria.amaris@gmail.com>
2016-11-04 14:44:17 -0500

    [UPD] cop_trm_rate: pep8 optimization

--- cop_trm_rate/models/cop_trm.py
+++ cop_trm_rate/models/cop_trm.py
@@ -20,18 +20,20 @@
 #
 ##############################################################################
 
-from openerp import models, api
-from suds.client import Client
 from datetime import datetime
-import xml.etree.ElementTree as ET
-import suds
 import logging
+import suds
+from suds.client import Client
+import xml.etree.ElementTree as ET
+
+from openerp import models, api
 
 _logger = logging.getLogger(__name__)
 
-BANREP_URL ='http://obiee.banrep.gov.co/analytics/saw.dll?wsdl'
+BANREP_URL = 'http://obiee.banrep.gov.co/analytics/saw.dll?wsdl'
 BANREP_USER = 'publico'
 BANREP_PASSWORD = 'publico'
+
 
 class trmColombian(models.Model):
     _inherit = 'res.currency.rate'
@@ -39,32 +41,32 @@ class trmColombian(models.Model):
     def _get_soap_trm(self):
         rate_name = False
         rate_value = 0.0
-        client = Client(BANREP_URL, service = 'SAWSessionService')
+        client = Client(BANREP_URL, service='SAWSessionService')
         session_id = client.service.logon(BANREP_USER, BANREP_PASSWORD)
-        client.set_options(service = 'XmlViewService')
+        client.set_options(service='XmlViewService')
         report = {
-            'reportPath': '/shared/Consulta Series Estadisticas desde Excel/'\
-            '1. Tasa de Cambio Peso Colombiano/1.1 TRM - Disponible desde '\
-            'el 27 de noviembre de 1991/1.1.3 Serie historica para un rango '\
-            'de fechas dado',\
+            'reportPath': '/shared/Consulta Series Estadisticas desde Excel/'
+            '1. Tasa de Cambio Peso Colombiano/1.1 TRM - Disponible desde '
+            'el 27 de noviembre de 1991/1.1.3 Serie historica para un rango '
+            'de fechas dado',
             'reportXml': 'null'
         }
         options = {
-            'async' : 'false',
-            'maxRowsPerPage' : '100',
-            'refresh' : 'true',
-            'presentationInfo' : 'true'
+            'async': 'false',
+            'maxRowsPerPage': '100',
+            'refresh': 'true',
+            'presentationInfo': 'true'
         }
         try:
             result_query = client.service.executeXMLQuery(
                 report, 'SAWRowsetData', options, session_id
-                )
-            client.set_options(service = 'SAWSessionService')
+            )
+            client.set_options(service='SAWSessionService')
             xml_data = ET.fromstring(result_query.rowset)
             rate_name = xml_data[0][1].text
             rate_value = float(xml_data[0][2].text)
         except suds.WebFault as detail:
-            _logger.warning('Error while fetching info from BancoRep API: ' 
+            _logger.warning('Error while fetching info from BancoRep API: '
                             + detail)
         client.service.logoff(session_id)
         return rate_name, rate_value
@@ -77,14 +79,15 @@ class trmColombian(models.Model):
         '''
         rate_name, trm = self._get_soap_trm()
         currency_id = self.env['res.currency'].search(
-            [('name','in',('USD','usd'))]
-            )[0].id
+            [('name', 'in', ('USD', 'usd'))]
+        )[0].id
         try:
-            old_trm = self.search([('currency_id','=',currency_id)], limit=1,
-                order='id desc')[0].rate
+            old_trm = self.search(
+                [('currency_id', '=', currency_id)],
+                limit=1, order='id desc')[0].rate
         except:
             old_trm = 0
-        if trm > 0 and trm != old_trm:  #Solo se actualiza si ha cambiado
+        if trm > 0 and trm != old_trm:  # Solo se actualiza si ha cambiado
             vals = {
                 'rate': trm,
                 'currency_id': currency_id,
