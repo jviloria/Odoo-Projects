c59f6d912c6fece5c04d5d74cde4c8df4bf2a54d
John W. Viloria Amaris <john.viloria.amaris@gmail.com>
2016-11-04 09:51:46 -0500

    [UPD] cop_trm_rate: pep8 modifications

--- cop_trm_rate/models/cop_trm.py
+++ cop_trm_rate/models/cop_trm.py
@@ -44,9 +44,9 @@ class trmColombian(models.Model):
         client.set_options(service = 'XmlViewService')
         report = {
             'reportPath': '/shared/Consulta Series Estadisticas desde Excel/'\
-            '1. Tasa de Cambio Peso Colombiano/1.1 TRM - Disponible desde el '\
-            '27 de noviembre de 1991/1.1.3 Serie historica para un rango de '\
-            'fechas dado',
+            '1. Tasa de Cambio Peso Colombiano/1.1 TRM - Disponible desde '\
+            'el 27 de noviembre de 1991/1.1.3 Serie historica para un rango '\
+            'de fechas dado',\
             'reportXml': 'null'
         }
         options = {
@@ -56,13 +56,16 @@ class trmColombian(models.Model):
             'presentationInfo' : 'true'
         }
         try:
-            result_query = client.service.executeXMLQuery(report, 'SAWRowsetData', options, session_id)
+            result_query = client.service.executeXMLQuery(
+                report, 'SAWRowsetData', options, session_id
+                )
             client.set_options(service = 'SAWSessionService')
             xml_data = ET.fromstring(result_query.rowset)
             rate_name = xml_data[0][1].text
             rate_value = float(xml_data[0][2].text)
         except suds.WebFault as detail:
-            _logger.warning('Error while fetching info from BancoRep API: ' + detail)
+            _logger.warning('Error while fetching info from BancoRep API: ' 
+                            + detail)
         client.service.logoff(session_id)
         return rate_name, rate_value
 
@@ -73,9 +76,12 @@ class trmColombian(models.Model):
         Hourly recomended
         '''
         rate_name, trm = self._get_soap_trm()
-        currency_id = self.env['res.currency'].search([('name','in',('USD','usd'))])[0].id
+        currency_id = self.env['res.currency'].search(
+            [('name','in',('USD','usd'))]
+            )[0].id
         try:
-            old_trm = self.search([('currency_id','=',currency_id)], limit=1, order='id desc')[0].rate
+            old_trm = self.search([('currency_id','=',currency_id)], limit=1,
+                order='id desc')[0].rate
         except:
             old_trm = 0
         if trm > 0 and trm != old_trm:  #Solo se actualiza si ha cambiado
