01444d221d80aa9604024f8b655fa2b564de77b6
John W. Viloria Amaris <john.viloria.amaris@gmail.com>
2016-11-13 21:00:05 -0500

    [UPD] base_vat_co: se refina la verificación del VAT y se extiende la lista de campos supervisados por el constraint, con la inclusión de country_id

--- base_vat_co/models/partner.py
+++ base_vat_co/models/partner.py
@@ -21,9 +21,7 @@
 
 from odoo import api, fields, models, _
 from odoo.exceptions import ValidationError
-import logging
 
-_logger = logging.getLogger(__name__)
 
 class ResPartnerDocumentType(models.Model):
     _name = 'res.partner.document.type'
@@ -32,21 +30,32 @@ class ResPartnerDocumentType(models.Model):
     name = fields.Char('Document Type', size=100, required=True)
     code = fields.Char('Code', size=2, required=True)
     checking_required = fields.Boolean('VAT Check Required',
-        default=False)
+                                       default=False)
 
 
 class ResPartner(models.Model):
     _inherit = ['res.partner']
 
     document_type_id = fields.Many2one('res.partner.document.type',
-        'Partner Document Type')
+                                       'Document Type')
 
-    @api.constrains('vat','document_type_id')
+    @api.constrains('vat', 'document_type_id', 'country_id')
     def check_vat(self):
         '''
         Check the VAT number depending of the country.
         http://sima-pc.com/nif.php
         '''
+        def _checking_required(partner):
+            '''
+            Este método solo aplica para Colombia y obliga a seleccionar
+            un tipo de documento de identidad con el fin de determinar
+            si es verificable por el algoritmo VAT. Si no se define,
+            de todas formas el VAT se evalua como un NIT/RUT.
+            '''
+            return ((partner.document_type_id and \
+                partner.document_type_id.checking_required) or \
+                not partner.document_type_id) == True
+
         msg = 'The Vat does not seems to be correct.'
         for partner in self:
             if not partner.vat:
@@ -59,14 +68,11 @@ class ResPartner(models.Model):
             if not hasattr(self, 'check_vat_' + vat_country):
                 continue
             check = getattr(self, 'check_vat_' + vat_country)
-            if partner.document_type_id:
-                _logger.critical("PRIMERA")
-                if partner.document_type_id.checking_required:
-                    if not check(vat_number):
-                        raise ValidationError(_(msg))
-            else:
-                if not check(vat_number):
-                    raise ValidationError(_(msg))
+            if vat_country == 'co':
+                if not _checking_required(partner):
+                    continue
+            if not check(vat_number):
+                raise ValidationError(_(msg))
         return True
 
     def check_vat_co(self, vat):
@@ -81,7 +87,7 @@ class ResPartner(models.Model):
             int(vat)
         except ValueError:
             return False
-        #Validación Nit Terceros del exterior
+        # Validación Nit Terceros del exterior
         if len(str(vat)) == 9 and str(vat)[0:5] == '44444' \
                 and int(str(vat)[5:]) <= 9000 \
                 and int(str(vat)[5:]) >= 4001:
