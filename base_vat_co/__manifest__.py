e68c2f95209d7a39ec4e1547c1c9687ce4f700d4
John W. Viloria Amaris <john.viloria.amaris@gmail.com>
2016-11-13 22:01:57 -0500

    [UPD] base_vat_co: se cambia la licencia en el __manifest__.py y se actualiza el texto de licencia en los restantes archivos .py

--- base_vat_co/__manifest__.py
+++ base_vat_co/__manifest__.py
@@ -1,23 +1,6 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    John W. Viloria Amaris <john.viloria.amaris@gmail.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# Copyright (C) 2016 John W. Viloria Amaris <john.viloria.amaris@gmail.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name":"Base Vat Colombia",
@@ -30,6 +13,7 @@ Module for Colombian Rut/Nit checking.
     "depends":['base','base_vat'],
     "category":"Invoice",
     "author":"John Viloria Amaris",
+    'license': 'AGPL-3',
     "data":[
         "partner_data.xml",
         "views/res_partner_view.xml"
